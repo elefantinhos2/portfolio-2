4ceee78388a81d03dddb7d05f86cbab5aa2a1547
elefantinhos2 <douglasramires01@gmail.com>
2022-10-27 14:09:46 -0200

    updated userData and portifolioUrl

--- src/utils/userData.ts
+++ src/utils/userData.ts
@@ -1,9 +1,9 @@
 export const userData = {
-  nameUser: "Samuel Leão",
-  githubUser: "samuelleaokenzie",
-  linkedinUser: "nekelpatrick",
-  whatsappNumber: "+5541988819028",
-  emailUser: "samuel@kenzie.com.br",
-  instagramUser: "samuelleaoui",
+  nameUser: "Douglas Leão",
+  githubUser: "elefantinhos2",
+  linkedinUser: "douglasramires",
+  whatsappNumber: "+5551989338367",
+  emailUser: "douglasramires01@gmail.com",
+  instagramUser: "",
   facebookUser: "",
 };
